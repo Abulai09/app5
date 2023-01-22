214f63107b6897fc6813822ff8568b0135092c6f
Abulai <116180549+Abulai09@users.noreply.github.com>
2023-01-22 21:39:16 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+
+  setTimeout(()=>{
+
+    document.body.classList.add('loaded')
+
+    Draggable.create('.gallery',{
+      bounds:'body',
+      inertia:true
+    })
+  })
+
+}
